a3778967e116831562c11a644b1eabbf6267bf0c
Joseph Gabereau <jgabereau@gmail.com>
2023-01-05 14:16:05 -0500

    header access control

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const {
 
 app.use(helmet())
 app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Methods',
     'OPTIONS, HEAD, GET, PUT, POST, DELETE'
