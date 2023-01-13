0e32279cf95cae00e36958497c8c6f62a98c12a3
Joseph Gabereau <jgabereau@gmail.com>
2023-01-13 14:12:10 -0500

    server with new enpoints

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const app = express();
 const helmet = require("helmet");
 
 const corsOptions = {
-  origin: "http://g-4.org",
-  credentials: true
-}
+  origin: "http://localhost:3000"
+} 
 
 require("dotenv").config();
 
@@ -17,9 +16,10 @@ const {
     postTeam,
     postEvent,
     deleteTeam,
+    deleteEvent,
+    updateEvent,
     updateTeam,
-    postSignIn,
-    helloWorld
+    postSignIn
 } = require("./handlers");
 
 app.use(helmet())
@@ -39,17 +39,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', express.static(__dirname + '/'));
 app.use(cors(corsOptions));
-app.get('/api/helloworld', helloWorld);
-app.get('/api/get_events', getEvents);
+app.get('/api/get_events', getEvents)
 app.get('/api/getg4team', getG4team);
 app.post('/api/auth/signin', postSignIn);
 app.post('/api/add_team', postTeam);
 app.post('/api/add_event', postEvent);
 app.post('/api/send_email', postEmail);
-app.patch('/api/update_team/:teamId', updateTeam)
+app.patch('/api/update_team/:teamId', updateTeam);
+app.patch('/api/update_event/:eventId', updateEvent);
 app.delete('/api/delete_team/:teamId', deleteTeam);
+app.delete('/api/delete_event/:eventId', deleteEvent);
+
 
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
